3852c4f529741d3ffef10d72ed578dd7bca281f8
ssabrrina <2302560D@student.tp.edu.sg>
2025-02-02 19:19:46 +0800

    Update create_report.php

--- assignment/report/create_report.php
+++ assignment/report/create_report.php
@@ -120,7 +120,7 @@
 </header>
 <nav>
     <ul>
-    <li><a href="../dashboard.php">Dashboard</a></li>
+        <li><a href="../dashboard.php">Dashboard</a></li>
         <li><a href="../vendors/vendor.php">Vendor</a></li>
         <li><a href="../inventory/inventory.php">Inventory Management</a></li>
         <li><a href="../procurement/procurement.php">Procurement Request</a></li>
