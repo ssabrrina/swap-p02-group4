23112ddfa1f8c608f37731f7951b983ea6c57c62
ssabrrina <2302560D@student.tp.edu.sg>
2025-02-02 19:16:16 +0800

    Update update_reportform.php

--- assignment/report/update_reportform.php
+++ assignment/report/update_reportform.php
@@ -152,6 +152,7 @@ $row = $result->fetch_assoc();
         <li><a href="../inventory/inventory.php">Inventory Management</a></li>
         <li><a href="../procurement/procurement.php">Procurement Request</a></li>
         <li><a href="../report/report.php">Report</a></li>
+        <li><a href="../order/read_orders.php">Purchase Orders</a></li>
         <li><a href="../logout.php">Log Out</a></li>
     </ul>
 </nav>
