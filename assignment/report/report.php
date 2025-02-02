eae6c1e582ad5cb376ad457fe60088a2e107b5d7
ssabrrina <2302560D@student.tp.edu.sg>
2025-02-02 19:15:50 +0800

    Update report.php

--- assignment/report/report.php
+++ assignment/report/report.php
@@ -116,6 +116,7 @@
         <li><a href="../inventory/inventory.php">Inventory Management</a></li>
         <li><a href="../procurement/procurement.php">Procurement Request</a></li>
         <li><a href="../report/report.php">Report</a></li>
+        <li><a href="../order/read_orders.php">Purchase Orders</a></li>
         <li><a href="../logout.php">Log Out</a></li>
     </ul>
 </nav>
